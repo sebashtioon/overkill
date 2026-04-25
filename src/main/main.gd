43b06fed44087dc52457aa10118f8e0dfaca8775
sebashtioon <sebastiansuciu607@gmail.com>
2026-04-25 23:58:13 +1000

    COOKERING

--- src/main/main.gd
+++ src/main/main.gd
@@ -1,1 +1,9 @@
 extends Node3D
+
+@export var overkill: AudioStreamPlayer
+
+
+func _ready() -> void:
+	overkill.play(45.0)
+	var tween = get_tree().create_tween()
+	tween.tween_property(overkill, "volume_db", 0.0, 3.0).from(-80.0)
