108e16b02f36a54d8f2e37c8b316382cc8e73164
sebashtioon <sebastiansuciu607@gmail.com>
2026-04-26 22:31:27 +1000

    efwefjhwiue

--- src/turret/turret.gd
+++ src/turret/turret.gd
@@ -3,6 +3,7 @@ extends Node3D
 @export var head: Node3D
 @export var camera: Camera3D
 
+var is_shaking : bool = false
 
 
 
@@ -14,3 +15,27 @@ func _handle_mouse_look(mouse_relative: Vector2) -> void:
 	head.rotate_y(-mouse_relative.x * 0.001)
 	camera.rotate_x(-mouse_relative.y * 0.001)
 	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+
+
+func shake_rot(max_rot_deg : float = 2.0, duration : float = 0.25) -> void:
+	if is_shaking: return
+	is_shaking = true
+	
+	var max_rot := deg_to_rad(max_rot_deg)
+	var time_left := duration
+	var start_rotation := camera.rotation
+	
+	while time_left > 0:
+		var offset_x = randf_range(-max_rot, max_rot)
+		var offset_y = randf_range(-max_rot, max_rot)
+		
+		camera.rotation.x = start_rotation.x + offset_x
+		camera.rotation.y = start_rotation.y + offset_y
+		
+		time_left -= get_process_delta_time()
+		await get_tree().process_frame
+		start_rotation.x = camera.rotation.x - offset_x
+	
+	camera.rotation = start_rotation
+	is_shaking = false
