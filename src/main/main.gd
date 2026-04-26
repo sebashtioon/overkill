c5b6aa9c5e65adc9a6cc7c61dbc84f261bac5ce6
sebashtioon <sebastiansuciu607@gmail.com>
2026-04-26 11:13:03 +1000

    man

--- src/main/main.gd
+++ src/main/main.gd
@@ -1,9 +1,11 @@
 extends Node3D
 
 @export var overkill: AudioStreamPlayer
+@export var cinematic_cam: Camera3D
 
 
 func _ready() -> void:
 	overkill.play(45.0)
+	cinematic_cam.make_current()
 	#var tween = get_tree().create_tween()
 	#tween.tween_property(overkill, "volume_db", 0.0, 3.0).from(-80.0)
