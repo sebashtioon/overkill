c21cb556eb57adcc1cc586f1a505204ebc65fd3f
sebashtioon <sebastiansuciu607@gmail.com>
2026-04-26 10:38:53 +1000

    mau5

--- src/globals/debug.gd
+++ src/globals/debug.gd
@@ -1,6 +1,6 @@
 extends Node
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if OS.is_debug_build():
 		if Input.is_action_just_pressed("dbg_quit"):
 			get_tree().quit()
