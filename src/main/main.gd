60c630c20d8045ff61c82a386a59853843377856
sebashtioon <sebastiansuciu607@gmail.com>
2026-04-26 16:14:55 +1000

    progress

--- src/main/main.gd
+++ src/main/main.gd
@@ -6,8 +6,8 @@ extends Node3D
 
 func _ready() -> void:
 	overkill.play(45.0)
-	#cinematic_cam.make_current()
+	cinematic_cam.make_current()
 	
 	
-	#var tween = get_tree().create_tween()
-	#tween.tween_property(overkill, "volume_db", 0.0, 3.0).from(-80.0)
+	var tween = get_tree().create_tween()
+	tween.tween_property(overkill, "volume_db", 0.0, 3.0).from(-80.0)
